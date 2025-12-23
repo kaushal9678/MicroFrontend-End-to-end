e7e2aafe8dbcfbae11e482fdcec931f5d60be8f4
yadav-kaushal <kaushlendra.yadav@protiviti.co.in>
2025-12-23 15:35:20 +0530

    Initial Commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,35 @@
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const moduleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common');
+const devConfig = {
+    mode:'development',
+   devServer:{
+        port:8080,
+        historyApiFallback:{
+            index:'index.html'
+        }
+    },
+    plugins:[
+        new moduleFederationPlugin({
+            name:'container',
+            remotes:{
+                marketing:'marketing@http://localhost:8081/remoteEntry.js'
+            },
+            shared:{
+                react:{
+                    singleton:true,
+                    requiredVersion:require('../package.json').dependencies.react
+                },
+                'react-dom':{
+                    singleton:true,
+                    requiredVersion:require('../package.json').dependencies['react-dom']
+                }
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+};
+   module.exports = merge(commonConfig,devConfig);
